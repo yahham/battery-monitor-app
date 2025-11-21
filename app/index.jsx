5d7d3bc3656e82f966816e3d356a3208b4194d72
Yahia Hamitou <hamitou.yah@gmail.com>
2025-11-21 09:23:30 +0100

    update Home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import Logo from "../assets/images/bma-logo.png";
 
 const Home = () => {
     return (
-        <View>
-            <Text>Battery Monitor App</Text>
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+
+            <Text style={styles.title}>Batter Monitor</Text>
+
+            <Text style={{ marginTop: 10, marginBottom: 30 }}>
+                Smart battery monitoring for better power efficiency.
+            </Text>
         </View>
     );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#e0dfe8",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    img: {
+        marginVertical: 20,
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+});
